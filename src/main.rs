de44951081b5a1e1c1ce93dbf37edff54f75fdec
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-08-23 17:07:39 +0430

    creating a window

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,20 @@
 #[macro_use]
 extern crate glium;
 
+use glium::glutin::{
+    dpi::LogicalSize, event_loop::EventLoop, window::WindowBuilder, ContextBuilder,
+};
+
 fn main() {
-    
+    let mut event_loop = EventLoop::new();
+    let context_builder = ContextBuilder::new();
+
+    let window_builder = WindowBuilder::new()
+        .with_inner_size(LogicalSize::new(620, 410))
+        .with_title("Vectoria");
+
+    let display = glium::Display::new(window_builder, context_builder, &event_loop)
+        .expect("failed to create Display object");
+
+        
 }
