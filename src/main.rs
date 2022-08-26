8180a2682d025d615a887c403df300293ac23d65
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-08-26 18:36:39 +0430

    camera implemented

--- src/main.rs
+++ src/main.rs
@@ -57,11 +57,13 @@ pub fn main() {
             out vec3 v_normal;
 
             uniform mat4 perspective;
-            uniform mat4 matrix;
+            uniform mat4 view;
+            uniform mat4 model;
 
             void main() {
-                v_normal = transpose(inverse(mat3(matrix))) * normal;
-                gl_Position = perspective * matrix * vec4(position, 1.0);
+                mat4 model_view = view * model;
+                v_normal = transpose(inverse(mat3(model_view))) * normal;
+                gl_Position = perspective * model_view * vec4(position, 1.0);
             }
         "#;
 
@@ -124,9 +126,10 @@ pub fn main() {
                 [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
             ]
         };
+        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
 
         let uniforms = uniform! {
-            matrix: [
+            model: [
                 [s,0.0,0.0,0.0],
                 [0.0,s,0.0,0.0],
                 [0.0,0.0,s,0.0],
@@ -134,6 +137,7 @@ pub fn main() {
             ],
             u_light: [-1.0 , 0.4 , 0.9f32],
             perspective: perspective,
+            view:view,
         };
 
         target_frame.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
@@ -161,4 +165,38 @@ pub fn main() {
 
         target_frame.finish().expect("failed to draw on screen");
     });
+}
+
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [up[1] * f[2] - up[2] * f[1],
+             up[2] * f[0] - up[0] * f[2],
+             up[0] * f[1] - up[1] * f[0]];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+             f[2] * s_norm[0] - f[0] * s_norm[2],
+             f[0] * s_norm[1] - f[1] * s_norm[0]];
+
+    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+             -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+             -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
 }
